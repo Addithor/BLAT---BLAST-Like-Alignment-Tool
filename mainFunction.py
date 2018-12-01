20040cd9a095d74ecde943e66ee49e0ce2a9c9c4
Arnar Þór Björgvinsson <addithor02@gmail.com>
2018-12-01 13:28:32 +0000

    Added measurment of the program runtime

--- mainFunction.py
+++ mainFunction.py
@@ -1,5 +1,6 @@
 from Bio.SeqIO.FastaIO import SimpleFastaParser
 from Bio import pairwise2
+import timeit
 
 from readSubseqFile import readGenome
 from readTranscriptsFile import readQuery
@@ -17,7 +18,10 @@ indexDict = {}
 indexDict = createIndex(genome, k)
 #commonIndexes(indexDict)
 
+total_time = 0
+j = 0
 for i in range(len(querys)):
+        start = timeit.default_timer()
         listOfHits = mapQueryToGenome(indexDict, k, querys[i])
 
         results = []
@@ -25,6 +29,7 @@ for i in range(len(querys)):
         results = callAlignment(genome, listOfHits, querys[i], -1)
         if not results:
                 print('alignment not found!')
+                j += 1
                 continue
 
         print(end='\n')
@@ -32,8 +37,15 @@ for i in range(len(querys)):
         print('--------------------------------------------------')
 
         i = 1
+        j += 1
         for item in results:
                 print('{0:2d} {1:10d} {2:10d} {3:6d} {4:9d} {5} {6}' .format(i, item[1], item[0][2], item[3], item[0][2] + item[3], '   ', item[2], end='\n'))
                 i += 1
 
-        
+        stop = timeit.default_timer()
+        time = (stop - start)
+        print('Runtime for query number ', j, ': ', '{0:2.3f}' .format(time), 's') 
+        total_time += time
+
+print(end='\n')        
+print('Total runtime for all queries: ', '{0:2.3f}' .format(total_time), 's') 
